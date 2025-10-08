0f7e9b1be3a705ff7a61d182b6ef3d2550b3bcf4
wyattmatt <wyatt.honny06@gmail.com>
2025-10-08 15:22:01 +0800

    checkpoint 4

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,10 @@ class MyApp extends StatelessWidget {
       title: 'Navigation Lab',
       initialRoute: '/',
       routes: {
-        '/': (context) => HomeScreen(),
+        '/': (context) => LoginScreen(),
         '/details': (context) => DetailsScreen(),
+        '/home': (context) => HomeScreen(),
+        '/profile': (context) => ProfileScreen(),
       },
     );
   }
@@ -19,18 +21,36 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as String?;
+
     return Scaffold(
       appBar: AppBar(title: Text('Home Screen')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(
-              context,
-              '/details',
-              arguments: 'Data from Home Screen',
-            );
-          },
-          child: Text('Open Details'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  '/details',
+                  arguments: 'Data from Home Screen',
+                );
+              },
+              child: Text('Open Details'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  '/profile',
+                  arguments: args ?? 'Anda',
+                );
+              },
+              child: Text('Open Profile'),
+            ),
+          ],
         ),
       ),
     );
@@ -49,6 +69,47 @@ class DetailsScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(args ?? 'No data received'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Back to Home'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/home', arguments: 'Kasmir');
+          },
+          child: Text('Login and Go Home'),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as String?;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Profile Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Wow, benar banget! $args memang tampan sekali'),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () => Navigator.pop(context),
